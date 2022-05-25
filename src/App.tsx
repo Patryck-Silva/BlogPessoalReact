7b8d8093abb824f410e1f6bca6142d2b77f13cec
Patryck Silva <patrycksilva70@gmail.com>
2022-05-25 19:34:42 -0300

    Adicionando navbar, footer e home todos estilizados

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import Navbar from './components/static/navbar/Navbar'
+import Footer from './components/static/footer/Footer'
 import './App.css';
 import Home from './pages/home/Home'
 
 function App() {
   return (
-    <Home />
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
   );
 }
 
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,16 +1,30 @@
-import React from "react";
+import React from 'react';
+import { Typography, Box, Grid, Button } from '@material-ui/core';
 import './Home.css';
 
 function Home() {
   return (
     <>
-      <h1 className="titulo">Home</h1>
-      <div className="div-imagem">
-        <img src="https://i0.wp.com/www.jbox.com.br/wp/wp-content/uploads/2021/12/Berserk-41-destacada.jpg?fit=774%2C489&quality=99&strip=all&ssl=1" className="imagem" alt="" />
-      </div>
+      <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "purple" }}>
+        <Grid alignItems="center" item xs={6}>
+          <Box paddingX={20} >
+            <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Seja bem vindo(a)!</Typography>
+            <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>expresse aqui os seus pensamentos e opiniões!</Typography>
+          </Box>
+          <Box display="flex" justifyContent="center">
+            <Box marginRight={1}>
+            </Box>
+            <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#3F51B5", color: "white" }}>Ver Postagens</Button>
+          </Box>
+        </Grid>
+        <Grid item xs={6} >
+          <img src="https://i.imgur.com/VlcpWAt.jpg" alt="" width="500px" height="500px" />
+        </Grid>
+        <Grid xs={12} style={{ backgroundColor: "white" }}>
+        </Grid>
+      </Grid>
     </>
   );
-
 }
 
 export default Home;
