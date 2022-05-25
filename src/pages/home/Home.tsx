f8dea77894abff2b9c7e813a607061e8f8e4cf0a
Patryck Silva <patrycksilva70@gmail.com>
2022-05-25 10:07:08 -0300

    Adicionando o novo projeto

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './Home.css';
+
+function Home() {
+  return (
+    <>
+      <h1 className="titulo">Home</h1>
+      <div className="div-imagem">
+        <img src="https://i0.wp.com/www.jbox.com.br/wp/wp-content/uploads/2021/12/Berserk-41-destacada.jpg?fit=774%2C489&quality=99&strip=all&ssl=1" className="imagem" alt="" />
+      </div>
+    </>
+  );
+
+}
+
+export default Home;
