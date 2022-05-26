f7d7189600c5c395e8256c33a2235ee05fc6bf9b
Patryck Silva <patrycksilva70@gmail.com>
2022-05-26 17:00:45 -0300

    Modificando os icones do footer para deixar bonitinho

--- src/components/static/footer/Footer.tsx
+++ src/components/static/footer/Footer.tsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import InstagramIcon from '@material-ui/icons/Instagram';
-import FacebookIcon from '@material-ui/icons/Facebook';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import { Typography, Box, Grid } from '@material-ui/core';
 
 function Footer() {
@@ -9,19 +6,19 @@ function Footer() {
     <>
       <Grid container direction="row" justifyContent="center" alignItems="center">
         <Grid alignItems="center" item xs={12}>
-          <Box style={{ backgroundColor: "purple", height: "120px" }}>
-            <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-              <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Siga-nos nas redes sociais </Typography>
+          <Box style={{ backgroundColor: "#220926", height: "120px" }}>
+            <Box paddingTop={1} paddingLeft={10} display="flex" alignItems="center" justifyContent="center">
+              <Typography variant="h5" align="center" gutterBottom style={{ color: "white", marginRight: "90px" }}>Siga-me nas redes sociais </Typography>
             </Box>
             <Box display="flex" alignItems="center" justifyContent="center">
-              <a href="https://www.facebook.com/generationbrasil" target="_blank">
-                <FacebookIcon style={{ fontSize: 60, color: "white" }} />
+              <a href="https://github.com/Patryck-Silva" target="_blank" style={{ marginRight: "10px" }} rel="noreferrer">
+                <img src="https://cdn-icons-png.flaticon.com/512/214/214513.png" alt="github" width="50px" />
               </a>
-              <a href="https://www.instagram.com/generationbrasil/" target="_blank">
-                <InstagramIcon style={{ fontSize: 60, color: "white" }} />
+              <a href="https://www.instagram.com/tyckupnext/" target="_blank" style={{ marginRight: "10px" }} rel="noreferrer">
+                <img src="https://cdn-icons-png.flaticon.com/512/2111/2111463.png" alt="instagram" width="50px" height="50px" />
               </a>
-              <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank">
-                <LinkedInIcon style={{ fontSize: 60, color: "white" }} />
+              <a href="https://www.linkedin.com/in/patryck-silva/" target="_blank" rel="noreferrer" >
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" width="50px" height="50px" alt='linkedin' />
               </a>
             </Box>
           </Box>
